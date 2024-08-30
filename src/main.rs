38b32c6ef271ba370232695936c95ad7c3f8cc66
J. Zane Cook <jzanecook@z90.studio>
2024-08-30 12:12:33 -0500

    docs: Added GPLv3 License

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Copyright: (c) 2024, J. Zane Cook <z@agentartificial.com>
+// GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 use language_detection::detect_language;
 use poise::serenity_prelude::{self as serenity, ComponentInteractionDataKind};
 use dotenv::dotenv;
